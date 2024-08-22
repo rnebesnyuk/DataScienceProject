6cc436eafa3aaa891368c2d04cc3193ce6ceded4
rnebesnyuk <98305190+rnebesnyuk@users.noreply.github.com>
2024-08-22 11:48:08 +0300

    Add files via upload
    cv added

---
+++ src/services/cv_service/lic_rec.py
@@ -0,0 +1,120 @@
+from ultralytics import YOLO
+import cv2
+
+from src.services.cv_service.sort.sort import *
+from src.services.cv_service.util import get_car, read_license_plate
+
+
+
+results = {}
+
+mot_tracker = Sort()
+
+# load models
+coco_model = YOLO('yolov8n.pt') #for cars detection
+license_plate_detector = YOLO('license_plate_detector.pt') #for license plates detection
+
+# load videostream/file
+#vid = cv2.VideoCapture('./mazda.mp4')
+# frame_count = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+# print(f"Total frames in video: {frame_count}")
+
+vehicles = [2, 3, 5, 7]
+
+processed_frames_count = 0
+reinit_threshold=3
+# Function to process frame (for both video and image)
+def process_frame(frame, frame_nmr, results, vehicles, mot_tracker):
+    global processed_frames_count
+    global reinit_threshold
+    results[frame_nmr] = {}
+
+    # Detect vehicles
+    detections = coco_model(frame)[0]
+    detections_ = []
+    for detection in detections.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = detection
+        if int(class_id) in vehicles:  # Ensure vehicles is a list of class IDs for vehicles
+            detections_.append([x1, y1, x2, y2, score])
+
+    # Track vehicles
+    track_ids = mot_tracker.update(np.asarray(detections_))
+
+    # Detect license plates
+    license_plates = license_plate_detector(frame)[0]
+    for license_plate in license_plates.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = license_plate
+
+        # Assign license plate to car
+        xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+
+        if car_id != -1:
+            # Crop license plate
+            license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
+
+            # Process license plate
+            license_plate_crop_ = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+
+            # kernel = np.ones((1, 1), np.uint8)
+            # license_plate_crop_ = cv2.dilate(license_plate_crop, kernel, iterations=1)  #license_plate_dilate
+            # license_plate_crop_ = cv2.erode(license_plate_crop_gray, kernel, iterations=1)    #license_plate_erode 
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+
+            # Apply CLAHE to the grayscale image
+            enhanced_image = clahe.apply(license_plate_crop_)
+            #license_plate_crop_ = cv2.adaptiveThreshold(license_plate_crop_, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+            license_plate_crop__ = cv2.equalizeHist(enhanced_image)
+            license_plate_crop___ = cv2.fastNlMeansDenoising(license_plate_crop__, None, 30, 7, 21)
+            #_, license_plate_crop_ = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY)
+
+            # Read license plate number
+            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop___)
+            print(license_plate_text)
+            cv2.imshow("Image", license_plate_crop_)
+            cv2.waitKey(0)
+
+            if license_plate_text is not None:
+                results[frame_nmr][car_id] = {
+                    'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                    'license_plate': {
+                        'bbox': [x1, y1, x2, y2],
+                        'text': license_plate_text,
+                        'bbox_score': score,
+                        'text_score': license_plate_text_score
+                    }
+                }
+
+# Main function to handle video or image
+def process_video_or_image(input_source, vehicles, mot_tracker, s_type):
+    results = {}
+    frame_nmr = -1
+    
+    if s_type=='vid':
+        # Video processing
+        vid = cv2.VideoCapture(input_source)
+        ret = True
+        while ret:
+            frame_nmr += 1
+            ret, frame = vid.read()
+            if ret:
+                process_frame(frame, 0, results, vehicles, mot_tracker)
+        vid.release()
+
+    elif s_type=='pic':
+        # Image processing
+        # frame_nmr = 0
+        frame = cv2.imread(input_source)
+        if frame is None:
+            raise ValueError("Failed to read image.")
+        process_frame(frame, 0, results, vehicles, mot_tracker)
+    else:
+        print("Could not read the file.")
+
+    return results
+
+# Example usage
+# input_source = './hir.jpg'  # Path to video or image file
+# results = process_video_or_image(input_source, vehicles, mot_tracker)
+
+# write results
+# write_csv(results, './test.csv')
---
+++ src/services/cv_service/visualize_image.py
@@ -0,0 +1,86 @@
+import ast
+import csv
+from io import BytesIO
+from skimage import io
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import ImageFont, ImageDraw, Image
+
+def draw_text_with_pil(image, text, license_number_score, position, font_path='arial.ttf', color=(0, 255, 0)):
+    """
+    Draw UTF-8 text on the image using PIL to handle non-ASCII characters.
+
+    Args:
+        image: OpenCV image.
+        text (str): Text to draw.
+        position (tuple): Position to draw the text (x, y).
+        font_path (str): Path to the TTF font file.
+        font_size (int): Font size for the text.
+        color (tuple): Text color in BGR format.
+    """
+    # Convert the OpenCV image (BGR) to a PIL image (RGB)
+    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    
+    # Load the font
+    font = ImageFont.truetype(font_path, size=50)
+    
+    # Create a drawing context
+    draw = ImageDraw.Draw(image_pil)
+
+    text_to_draw = f'{text} ({license_number_score:.2f})'
+    
+    # Draw the text using PIL
+    draw.text(position, text_to_draw, font=font, fill=color)
+    
+    # Convert back to OpenCV format (BGR)
+    image = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
+    
+    return image
+
+
+
+def draw_bboxes_from_csv(image_path, csv_path, output_path):
+    """
+    Draw bounding boxes and scores on the image from the CSV file.
+
+    Args:
+        image_path (str): Path to the original image.
+        csv_path (str): Path to the CSV file containing bounding box data.
+        output_path (str): Path to save the image with drawn bounding boxes.
+    """
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Open the CSV file and read bounding box data
+    with open(csv_path, 'r', encoding='utf-16') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            # Parse car bounding box
+            car_bbox_str = row['car_bbox'].strip('[]').split()
+            car_bbox = [int(float(val)) for val in car_bbox_str]
+            car_id = row['car_id']
+            
+            # Parse license plate bounding box
+            lp_bbox_str = row['license_plate_bbox'].strip('[]').split()
+            lp_bbox = [int(float(val)) for val in lp_bbox_str]
+            license_number = row['license_number']
+            license_number_score = float(row['license_number_score'])
+            
+            # Draw car bounding box
+            cv2.rectangle(image, (car_bbox[0], car_bbox[1]), (car_bbox[2], car_bbox[3]), (255, 0, 0), 3)
+            cv2.putText(image, f'Car ID: {car_id}', (car_bbox[0], car_bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
+            
+            # Draw license plate bounding box
+            cv2.rectangle(image, (lp_bbox[0], lp_bbox[1]), (lp_bbox[2], lp_bbox[3]), (0, 255, 0), 3)
+            image = draw_text_with_pil(image, license_number, license_number_score, (lp_bbox[0], lp_bbox[1] - 50))
+            # cv2.putText(image, f'({license_number_score:.2f})', 
+            #             (lp_bbox[0], lp_bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    
+    # Save the image with drawn bounding boxes
+    cv2.imwrite(output_path, image)
+    print(f"Output saved to {output_path}")
+
+# Example usage
+#draw_bboxes_from_csv('hir.jpg', 'test.csv', 'test_b.jpg')
