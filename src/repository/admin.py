92a3ce9e5ecc22d6e02fe51dbc9daadad9c7b2aa
Serghii777 <142201494+Serghii777@users.noreply.github.com>
2024-08-26 21:26:47 +0200

    54321

--- src/repository/admin.py
+++ src/repository/admin.py
@@ -2,88 +2,38 @@ import csv
 from datetime import datetime
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, func
+from sqlalchemy import select
 from src.schemas.admin import ParkingRateCreate
-from src.models.models import BlackListCar, User, Vehicle, ParkingRecord, ParkingRate
-from fastapi import Depends
-from src.database.db import get_db
-from src.models.models import User, Role
-from src.schemas.user import UserCreateSchema
-from libgravatar import Gravatar
+from src.models.models import BlackListCar, ParkingLot, User, Vehicle, ParkingRecord, ParkingRate
 
-
-async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)) -> User | None:
+async def get_user_by_email(email: str, db: AsyncSession) -> User | None:
     query = select(User).filter_by(email=email)
     user = await db.execute(query)
     return user.scalar_one_or_none()
 
-
-# async def update_token(user: User, token: str | None, db: AsyncSession):
-#     user.refresh_token = token
-#     await db.commit()
-
-
-# async def confirmed_email(email: str, db: AsyncSession):
-#     user = await get_user_by_email(email, db)
-#     user.confirmed = True
-#     user.is_active = True
-#     await db.commit()
-
-
-# async def update_password(user: User, new_password: str, db: AsyncSession) -> User:
-#     user.password = new_password
-#     await db.commit()
-#     await db.refresh(user)
-#     return user
-
-
-# class UserRepository:
-#     def __init__(self, db: AsyncSession):
-#         self.db = db
-
-#     async def get_user_by_email(self, email: str):
-#         result = await self.db.execute(select(User).where(User.email == email))
-#         return result.scalar_one_or_none()
-
-#     async def create_user(self, user: User):
-#         self.db.add(user)
-#         await self.db.commit()
-#         await self.db.refresh(user)
-#         return user
-
-
-
-def __init__(self, db: AsyncSession):
-    self.db = db
-
-async def get_vehicle_by_license_plate(self, license_plate: str):
-    result = await self.db.execute(select(Vehicle).where(Vehicle.license_plate == license_plate))
+async def get_vehicle_by_license_plate(license_plate: str, db: AsyncSession):
+    result = await db.execute(select(Vehicle).where(Vehicle.license_plate == license_plate))
     return result.scalar_one_or_none()
 
-async def create_vehicle(self, vehicle: Vehicle):
-    self.db.add(vehicle)
-    await self.db.commit()
-    await self.db.refresh(vehicle)
+async def create_vehicle(vehicle: Vehicle, db: AsyncSession):
+    db.add(vehicle)
+    await db.commit()
+    await db.refresh(vehicle)
     return vehicle
 
-async def is_vehicle_registered(self, license_plate: str) -> bool:
-    vehicle = await self.get_vehicle_by_license_plate(license_plate)
+async def is_vehicle_registered(license_plate: str, db: AsyncSession) -> bool:
+    vehicle = await get_vehicle_by_license_plate(license_plate, db)
     return vehicle is not None
 
-
-
-def __init__(self, db: AsyncSession):
-    self.db = db
-
-async def create_parking_record(self, parking_record: ParkingRecord):
-    self.db.add(parking_record)
-    await self.db.commit()
-    await self.db.refresh(parking_record)
+async def create_parking_record(parking_record: ParkingRecord, db: AsyncSession):
+    db.add(parking_record)
+    await db.commit()
+    await db.refresh(parking_record)
     return parking_record
 
-async def get_parking_duration(self, vehicle_id: uuid.UUID) -> int | None:
+async def get_parking_duration(vehicle_id: uuid.UUID, db: AsyncSession) -> int | None:
     """Повертає тривалість паркування в хвилинах для поточного паркування"""
-    result = await self.db.execute(
+    result = await db.execute(
         select(ParkingRecord)
         .where(ParkingRecord.vehicle_id == vehicle_id)
         .order_by(ParkingRecord.entry_time.desc())
@@ -94,29 +44,47 @@ async def get_parking_duration(self, vehicle_id: uuid.UUID) -> int | None:
         duration = (parking_record.exit_time - parking_record.entry_time).total_seconds() // 60
         return int(duration)
     return None
-    
 
-async def generate_parking_report(vehicle_id: uuid.UUID, db: AsyncSession):
+async def generate_parking_report(license_plate: str, db: AsyncSession) -> str:
+    vehicle_result = await db.execute(
+        select(Vehicle.id)
+        .where(Vehicle.license_plate == license_plate)
+    )
+    vehicle = vehicle_result.scalar_one_or_none()
+    
+    if vehicle is None:
+        return None
+
     result = await db.execute(
         select(ParkingRecord)
-        .where(ParkingRecord.vehicle_id == vehicle_id)
+        .where(ParkingRecord.vehicle_id == vehicle)
         .order_by(ParkingRecord.entry_time)
     )
-        
+
     parking_records = result.scalars().all()
-        
+
     if not parking_records:
         return None
-    filename = f"parking_report_{vehicle_id}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
-    fields = ['Час в’їзду', 'Час виїзду', 'Тривалість (хв)', 'Вартість (грн)']
+
+    # Створення імені файлу
+    filename = f"parking_report_{license_plate}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+    fields = ['Entry Time', 'Exit Time', 'Duration (minutes)', 'Cost (currency)']
+
+    # Запис в CSV
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
         for record in parking_records:
-            csvwriter.writerow([record.entry_time, record.exit_time, record.duration, record.cost])
-        
-    return filename 
-    
+            csvwriter.writerow([
+                record.entry_time,
+                record.exit_time,
+                record.duration,
+                record.cost
+            ])
+
+    return filename
+
+
 async def save_parking_rate(
     rate_data: ParkingRateCreate,
     db: AsyncSession
@@ -130,15 +98,19 @@ async def save_parking_rate(
     await db.commit()
     await db.refresh(new_rate)
     return new_rate
-  
-class BlacklistRepository:
-    async def add_to_blacklist(self, license_plate: str):
-        blacklisted_entry = BlackListCar(license_plate=license_plate)
-        self.db.add(blacklisted_entry)
-        await self.db.commit()
-        return blacklisted_entry
 
-    async def is_blacklisted(self, license_plate: str) -> bool:
-        query = select(BlackListCar).where(BlackListCar.license_plate == license_plate)
-        result = await self.db.execute(query)
-        return result.scalar_one_or_none() is not None
+async def get_parking_spaces(db: AsyncSession) -> ParkingLot | None:
+    result = await db.execute(select(ParkingLot))
+    parking_lot = result.scalars().first()
+    return parking_lot
+
+async def add_to_blacklist(license_plate: str, db: AsyncSession):
+    blacklisted_entry = BlackListCar(license_plate=license_plate)
+    db.add(blacklisted_entry)
+    await db.commit()
+    return blacklisted_entry
+
+async def is_blacklisted(license_plate: str, db: AsyncSession) -> bool:
+    query = select(BlackListCar).where(BlackListCar.license_plate == license_plate)
+    result = await db.execute(query)
+    return result.scalar_one_or_none() is not None
--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1,21 +1,17 @@
-from typing import Optional
-from fastapi import APIRouter, Form, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-
-from src.repository import admin as repository_admin
 from src.schemas.admin import ParkingLotUpdate, ParkingRateCreate, UserRoleUpdate, UserStatusUpdate, VehicleCheckSchema
 from src.schemas.user import UserReadSchema
 from src.models.models import User, Role
 from src.services.role import RolesAccess
 from src.services.auth import auth_service
 from src.database.db import get_db
+from src.repository import admin as repository_admin
 from src.repository import users as repository_users
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 access_admin = RolesAccess([Role.admin])
-
 
 @router.patch("/users/block", dependencies=[Depends(access_admin)])
 async def change_user_status_by_email(
@@ -29,7 +25,6 @@ async def change_user_status_by_email(
     await repository_admin.change_user_status(user, body.is_active, db)
     return {"message": f"User status changed to {'active' if body.is_active else 'banned'}."}
 
-
 @router.patch("/users/unblock", dependencies=[Depends(access_admin)])
 async def unblock_user_by_email(
     body: UserStatusUpdate = Depends(),
@@ -41,7 +36,6 @@ async def unblock_user_by_email(
         raise HTTPException(status_code=404, detail="User not found")
     await repository_admin.change_user_status(user, body.is_active, db)
     return {"message": f"User status changed to {'active' if body.is_active else 'banned'}."}
-
 
 @router.patch("/{user_id}/change_role", dependencies=[Depends(access_admin)])
 async def update_user_role(
@@ -55,7 +49,6 @@ async def update_user_role(
     await repository_admin.update_user_role(user, body.role, db)
     return {"message": f"User role updated to {body.role}"}
 
-
 @router.post("/parking-rates", response_model=ParkingRateCreate, dependencies=[Depends(access_admin)])
 async def create_parking_rate(
     rate_data: ParkingRateCreate,
@@ -63,45 +56,34 @@ async def create_parking_rate(
     db: AsyncSession = Depends(get_db)
 ):
     try:
-        rate = await repository_admin.save_parking_rate(
+        created_rate = await repository_admin.save_parking_rate(
             rate_data=rate_data,
             db=db
         )
-        return rate
+        return created_rate
     except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
 
-
-@router.put("/parking-lot", response_model=ParkingLotUpdate, dependencies=[Depends(access_admin)])
-async def update_parking_spaces(
-    lot_data: ParkingLotUpdate,
+@router.get("/parking-lot", response_model=ParkingLotUpdate, dependencies=[Depends(access_admin)])
+async def get_parking_spaces(
     _: User = Depends(auth_service.get_current_user),
     db: AsyncSession = Depends(get_db)
 ):
-    parking_lot = await repository_admin.update_parking_spaces(
-        total_spaces=lot_data.total_spaces,
-        available_spaces=lot_data.available_spaces,
-        db=db
-    )
-    return parking_lot
-
+    parking_spaces = await repository_admin.get_parking_spaces(db=db)
+    if not parking_spaces:
+        raise HTTPException(status_code=404, detail="Parking lot not found")
+    return parking_spaces
 
 @router.put("/parking-info", response_model=VehicleCheckSchema, dependencies=[Depends(access_admin)])
 async def update_parking_info(
-    lot_data: VehicleCheckSchema,  
+    lot_data: VehicleCheckSchema,
     db: AsyncSession = Depends(get_db),
     _: User = Depends(auth_service.get_current_user),
 ):
-    parking_info = await repository_admin.update_parking_info(
-        total_spaces=lot_data.total_spaces,
-        available_spaces=lot_data.available_spaces,
-        rate_per_hour=lot_data.rate_per_hour,  
-        max_daily_rate=lot_data.max_daily_rate,
-        currency=lot_data.currency,
-        db=db
-    )
-    return parking_info
-
+    filename = await repository_admin.generate_parking_report(lot_data.license_plate, db)
+    if not filename:
+        raise HTTPException(status_code=404, detail="No records found for the given license plate.")
+    return {"filename": filename}
 
 @router.post("/add_to_blacklist", dependencies=[Depends(access_admin)])
 async def add_to_blacklist(
@@ -109,9 +91,8 @@ async def add_to_blacklist(
     db: AsyncSession = Depends(get_db),
     _: User = Depends(auth_service.get_current_user),
 ):
-    blacklist_repo = repository_admin.BlackListRepository(db)
-    if await blacklist_repo.is_blacklisted(license_plate):
+    if await repository_admin.is_blacklisted(license_plate):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Vehicle is already blacklisted.")
     
-    blacklisted_entry = await blacklist_repo.add_to_blacklist(license_plate)
-    return blacklisted_entry
+    blacklisted_entry = await repository_admin.add_to_blacklist(license_plate)
+    return {"message": "Vehicle added to blacklist.", "license_plate": blacklisted_entry.license_plate}
